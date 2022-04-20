e424fba22342a5efbe34ac74ea186349fbccaa20
Dimasbdd <91084191+Dimasbdd@users.noreply.github.com>
2022-04-20 19:58:20 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idAudio) {
+
+    document.querySelector(idAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+let contador = 0;
+
+//enquanto repetição
+while (contador < listaDeTeclas.length) {
+
+    listaDeTeclas[contador].onclick = function () {
+        tocaSom('#som_tecla_pom');
+    }
+
+    contador = contador + 1;
+}
